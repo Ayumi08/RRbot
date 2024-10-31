f2c008949d44f0fc10b988e544acfe91cbf6f1ac
Ayumi08 <ayuminakayama265@gmail.com>
2024-10-31 11:39:24 -0700

    warframe moment

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if not os.environ.get("TOKEN"):
 client = interactions.Client(
     token=os.environ.get("TOKEN"),
     activity=interactions.Activity(
-        name="with interactions", type=interactions.ActivityType.PLAYING
+        name="Warframe ofc", type=interactions.ActivityType.PLAYING
     ),
     debug_scope=DEV_GUILD,
 )
